8de4f73c682facfafe85e63d1e428b2bf900de6b
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-04-06 00:20:21 -0300

    Automatizando a atualização do carrinho usando useEffect

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { Product } from '../types';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -31,6 +31,20 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
     return [];
   });
+
+  const prevCartRef = useRef<Product[]>();
+
+  useEffect(() => {
+    prevCartRef.current = cart;
+  });
+
+  const cartPreviousValue = prevCartRef.current ?? cart;
+
+  useEffect(() => {
+    if (cartPreviousValue !== cart) {
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
+    }
+  }, [cart, cartPreviousValue])
 
   const addProduct = async (productId: number) => {
     try {
@@ -64,8 +78,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
       setCart(updatedCart);
 
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
-
     } catch {
       toast.error('Erro na adição do produto');
     }
@@ -81,8 +93,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         updatedCart.splice(productIndex, 1);
 
         setCart(updatedCart);
-
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
 
         return;
       }
@@ -119,8 +129,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         product.amount = amount;
 
         setCart(updatedCart);
-
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
 
         return;
       }
